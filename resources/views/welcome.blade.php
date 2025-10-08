9d325f3bdd043df9df90286cdca0f7ed6535fef0
PhantomCorner <jiaojiaobigbang@gmail.com>
2025-10-08 20:00:54 +1300

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <h1>Welcome to the Ninja Network</h1>
+        <p>click the button below to get started</p>
+        <a hrer="">
+            <button> Find Ninjas</button>
+        </a>
+    </body>
+
+</html>
